809290b1d8783cacd0a14db7aef0ed8f0d9c1e93
uraymeiviar <uraymeiviar@gmail.com>
2021-01-19 04:58:31 +0700

    launcher scene and script no longer needed, merged with NetGameManager

--- Assets/Scripts/impl/NetGameManager.cs
+++ Assets/Scripts/impl/NetGameManager.cs
@@ -2,14 +2,48 @@
 using Photon.Realtime;
 using Photon.Pun;
 using System.Collections.Generic;
+using System;
 
 namespace UniTank
 {
-    public class NetGameManager : GameManager
+    public class NetGameManager : GameManager, IConnectionCallbacks, IMatchmakingCallbacks
     {
+        public string gameVersion = "1.0";
+        public string playerName = "";
         public bool isHost = false;
         public GameObject tankPrefab;
         protected Dictionary<string, NetTankPlayer> playerIdMap = new Dictionary<string, NetTankPlayer>();
+        public new void Start()
+        {
+            Connect();
+        }
+
+        public void Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                Debug.Log("Launcher conncted to server!, joining random room");
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                Debug.Log("Launcher connecting to server...");
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = this.gameVersion;
+            }
+        }
+
+        void Awake()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+            PhotonNetwork.AutomaticallySyncScene = true;
+            if (this.playerName == "")
+            {
+                this.playerName = Environment.MachineName + "/" + Environment.UserName;
+            }
+            PhotonNetwork.LocalPlayer.NickName = this.playerName;
+        }
+
         public bool NetPlayerJoin(Player netPlayer, Tank tank = null)
         {
             if(tank == null && netPlayer.IsLocal)
@@ -76,6 +110,79 @@ namespace UniTank
         public override void DestroyObject(GameObject obj)
         {
             PhotonNetwork.Destroy(obj);
+        }
+
+        public void OnConnected()
+        {
+            Debug.Log("Connected");
+        }
+
+        public void OnConnectedToMaster()
+        {
+            Debug.Log("Connected to master, joining random room");
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.Log("Disconnected with reason " + cause.ToString());
+        }
+
+        public void OnRegionListReceived(RegionHandler regionHandler)
+        {
+            Debug.Log(regionHandler.ToString());
+        }
+
+        public void OnCustomAuthenticationResponse(Dictionary<string, object> data)
+        {            
+        }
+
+        public void OnCustomAuthenticationFailed(string debugMessage)
+        {            
+        }
+
+        public void OnFriendListUpdate(List<FriendInfo> friendList)
+        {            
+        }
+
+        public void OnCreatedRoom()
+        {
+            Debug.Log("Room Created");
+        }
+
+        public void OnCreateRoomFailed(short returnCode, string message)
+        {
+            Debug.Log("Failed to create room (" + returnCode.ToString() + ") message: " + message);
+        }
+
+        public void OnJoinedRoom()
+        {
+            base.Start();
+            if (this.NetPlayerJoin(PhotonNetwork.LocalPlayer))
+            {
+                Debug.Log("Player Count " + this.GetPlayerCount().ToString());
+            }
+        }
+
+        public void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("Join random room failed (" + returnCode.ToString() + ") message: " + message);
+            Debug.Log("Creating new room...");
+            PhotonNetwork.CreateRoom(null, new RoomOptions
+            {
+                MaxPlayers = (byte)this.config.maxPlayer,
+                PublishUserId = true
+            });
+        }
+
+        public void OnLeftRoom()
+        {
+            Debug.Log("Left Room.");
+        }
+
+        public void OnJoinRoomFailed(short returnCode, string message)
+        {
+            Debug.Log("Join room failed code " + returnCode.ToString() + " message: " + message);
         }
     }
 }
